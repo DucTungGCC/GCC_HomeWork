3ae5c200c91caf053e917381641dd9d50851fab5
zKay <ductung.ptit@gmail.com>
2025-11-02 17:29:06 +0700

    Fix Anim

--- GCC_BTVN/Assets/_Game/Scripts/Player/PlayerAnim.cs
+++ GCC_BTVN/Assets/_Game/Scripts/Player/PlayerAnim.cs
@@ -1,44 +1,101 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
-
+public enum AnimState {
+    Idle,
+    Run,
+    Jump,
+    Attack
+}
 public class PlayerAnim : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     [SerializeField] private PlayerMovement _playerMovement;
     [SerializeField] private PlayerAttack _playerAttack;
     [SerializeField] private Rigidbody2D _rb;
+
+    const string Anim_IDLE = "idle";
+    const string Anim_RUN = "run";
+    const string Anim_JUMP = "jump";
+    const string Anim_ATTACK = "attack";
+    const string Anim_JUMPSTATE = "velocityY";
+    string currentAnim = Anim_IDLE;
+    private AnimState currState = AnimState.Idle;
+    private float JumpState = 0f;
     
-    public bool isRunning = false;
-    public bool isJumping = false;
-    public float JumpState = 0;
-    public bool isAttacking = false;
+    private void LateUpdate()
+    {
+        CallAnim();
+    }
+
     private void Update()
     {
         UpdateState();
-        Movement();
+        JumpState = _rb.velocity.y;
     }
 
     private void UpdateState()
     {
-        isRunning = _playerMovement._moveDirection != 0;
-        isJumping = !_playerMovement.isGrounded();
-        JumpState = _rb.velocity.y;
-        isAttacking = _playerAttack.isAttacking;
+        if (_playerMovement._moveDirection != 0 || (_rb.velocity.y != 0 && _rb.velocity.x != 0))
+        {
+            currState = AnimState.Idle;
+            //ChangeAnim(Anim_IDLE);
+        }
+
+        if (_rb.velocity.x != 0 && _playerMovement.isGrounded())
+        {
+            currState = AnimState.Run;
+            //ChangeAnim(Anim_RUN);
+        }
+        else if (!_playerMovement.isGrounded())
+        {
+            currState = AnimState.Jump;
+        }
+        else
+        {
+            currState = AnimState.Idle;
+        }
+        if (_playerAttack.isAttacking)
+        {
+            currState = AnimState.Attack;
+            //ChangeAnim(Anim_ATTACK);
+        }
     }
 
-    private void Movement()
+    private void CallAnim()
     {
-        _animator.SetBool("isRunning", isRunning);
-        _animator.SetFloat("velocityY", JumpState);
-        _animator.SetBool("isJumping", isJumping);
+        switch (currState)
+        {
+            case AnimState.Attack:
+                ChangeAnim(Anim_ATTACK);
+                break;
+            case AnimState.Run:
+                ChangeAnim(Anim_RUN);
+                break;
+            case AnimState.Jump:
+                ChangeAnim(Anim_JUMP);
+                break;
+            default:
+                ChangeAnim(Anim_IDLE);
+                break;
+        }
+        
     }
     
-
-    public void Attack()
+    private void ChangeAnim(string newAnim)
     {
-        _animator.SetTrigger("isAttacking");
+        if (newAnim == Anim_JUMP)
+        {
+            _animator.SetFloat(Anim_JUMPSTATE, JumpState);
+        }
+        if (currentAnim != newAnim)
+        {
+            _animator.ResetTrigger(currentAnim);
+            currentAnim = newAnim;
+            _animator.SetTrigger(currentAnim);
+        }
     }
 }
